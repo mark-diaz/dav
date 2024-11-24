8db5a67d5f0bdf9dacb032a81853ea957ed09017
Mark Diaz <mldiaz2004@gmail.com>
2024-11-23 16:25:07 -0800

    batman

---
+++ mini_alu/miniALU.sv
@@ -0,0 +1,13 @@
+module miniALU (
+    input [3:0] op1,
+    input [3:0] op2, 
+    input operation,
+    input sign,
+    output [19:0] result
+    );
+
+    always_comb begin
+        
+
+    end
+endmodule
